36c778366be6df1d05820fadcae7cfa6eea9e119
Shohei Aoki <shoaok@gmail.com>
2016-10-17 12:36:48 +0900

    fix

--- convert2json.sh
+++ convert2json.sh
@@ -13,8 +13,8 @@ sed -i -e 's/\(.*\) = \([0-9,-.]*\)$/"\1" : \2/g' convert.json
 sed -i -e 's/\(.*\) = \(.*\)/"\1" : "\2"/g' convert.json 
 sed -i -e 's/\(.*\) : $/\1 : ""/g' convert.json 
 sed -i -e 's/$/,/g' convert.json
-sed -i -e "1s/^/'[{/" convert.json 
-sed -i -e '$s/,$/}]'\''/' convert.json 
+sed -i -e "1s/^/[{/" convert.json 
+sed -i -e '$s/,$/}]/' convert.json 
 sed -i -e 's/\n//g' convert.json
 perl -pe 's/\n/ /g' convert.json > ${basename}.json
 rm convert.json
