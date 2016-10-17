2f3440113fc06dbfa8043275d8574cada0ae04ce
Shohei Aoki <shoaok@gmail.com>
2016-10-17 12:12:05 +0900

    initial commit

---
+++ convert2json.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ $# -ne 1 ]; then
+  echo "Usage: ./convert2json.sh filename" 1>&2
+  exit 1
+fi
+
+filename=$1
+basename=${filename##*/}
+
+sed -e '/^#/d' ${filename} > convert.json
+sed -i -e 's/;.*//g' convert.json 
+sed -i -e 's/\(.*\) = \([0-9,-.]*\)$/"\1" : \2/g' convert.json 
+sed -i -e 's/\(.*\) = \(.*\)/"\1" : "\2"/g' convert.json 
+sed -i -e 's/\(.*\) : $/\1 : ""/g' convert.json 
+sed -i -e 's/$/,/g' convert.json
+sed -i -e "1s/^/'[{/" convert.json 
+sed -i -e '$s/,$/}]'\''/' convert.json 
+sed -i -e 's/\n//g' convert.json
+perl -pe 's/\n/ /g' convert.json > ${basename}.json
+rm convert.json
